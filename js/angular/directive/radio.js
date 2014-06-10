ionic-team/ionic-framework

commit 521164db786a0b836b5b8149816f50da55c6a82a

ngValue uses a value instead of variable

js/angular/directive/radio.js
@@ -24,7 +24,7 @@ IonicModule
     require: '?ngModel',
     scope: {
       ngModel: '=?',
-      ngValue: '=?',
+      ngValue: '@',
       ngChange: '&',
       icon: '@',
       name: '@'

Ryu9425/ang_ionic

commit d7edfc93bce64120bd412fe3d1b2c0346cb491a9

fix(radioButtons): Correcting a bug introduced by 521164db786a0b836b5b8149816f50da55c6a82a. Closes #1599

js/angular/directive/radio.js
@@ -24,7 +24,7 @@ IonicModule
     require: '?ngModel',
     scope: {
       ngModel: '=?',
-      ngValue: '@',
+      ngValue: '=?',
       ngChange: '&',
       icon: '@',
       name: '@'